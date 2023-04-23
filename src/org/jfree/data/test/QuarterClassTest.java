7175b5b2b9f95e454842990e12f296bba327fa0d
Yousef <yousef.adam2001@gmail.com>
2023-04-24 00:48:43 +0200

    tested more methods

--- src/org/jfree/data/test/QuarterClassTest.java
+++ src/org/jfree/data/test/QuarterClassTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import java.util.Date;
 import java.util.TimeZone;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class QuarterClassTest {
     Quarter quarter;
@@ -145,4 +145,121 @@ public class QuarterClassTest {
         assertEquals(8024, quarter.getSerialIndex());
     }
 
+    @Test
+    public void testGetSerialIndex2() {
+        arrange();
+        Quarter quarter2 = new Quarter(4, 2019);
+
+        Long serial1 = quarter.getSerialIndex();
+        Long serial2 = quarter2.getSerialIndex();
+
+        assertNotEquals(serial1, serial2);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //equals
+
+    @Test
+    public void testEquals() {
+        arrange();
+        Quarter quarter2 = new Quarter(2,2023);
+        assertTrue(quarter.equals(quarter2));
+    }
+
+    @Test
+    public void testEquals2() {
+        arrange();
+        Quarter quarter2 = new Quarter(4,2021);
+        assertFalse(quarter.equals(quarter2));
+    }
+
+    @Test
+    public void testEquals3() {
+        arrange();
+        Quarter quarter2 = new Quarter(4,2023);
+        assertFalse(quarter.equals(quarter2));
+    }
+
+    @Test
+    public void testEquals4() {
+        arrange();
+        Quarter quarter2 = new Quarter(2,2020);
+        assertFalse(quarter.equals(quarter2));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //hashCode
+
+    @Test
+    public void testHashCode() {
+        arrange();
+        assertNotNull(quarter.hashCode());
+    }
+
+    @Test
+    public void testHashCode2() {
+        arrange();
+        Quarter quarter2 = new Quarter(2,2020);
+        assertFalse(quarter.hashCode() == quarter2.hashCode());
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //compareTo
+
+    // -/+ the differences in the year first, then the quarters
+
+    @Test
+    public void testCompareTo() {
+        arrange();
+        //the same year
+        Quarter quarter2 = new Quarter(2,2023);
+        assertEquals(0, quarter.compareTo(quarter2));
+    }
+
+    @Test
+    public void testCompareTo2() {
+        arrange();
+        //a qurater after
+        Quarter quarter2 = new Quarter(3,2023);
+        assertEquals(-1, quarter.compareTo(quarter2));
+    }
+
+    @Test
+    public void testCompareTo3() {
+        arrange();
+        //a year after
+        Quarter quarter2 = new Quarter(2,2024);
+        assertEquals(-1, quarter.compareTo(quarter2));
+    }
+
+    @Test
+    public void testCompareTo4() {
+        arrange();
+        //14 years ago
+        Quarter quarter2 = new Quarter(2,2009);
+        assertEquals(14, quarter.compareTo(quarter2));
+    }
+
+    @Test
+    public void testCompareTo5() {
+        arrange();
+        //a quarter ago
+        Quarter quarter2 = new Quarter(1,2023);
+        assertEquals(1, quarter.compareTo(quarter2));
+    }
+
+    @Test
+    public void testCompareTo6() {
+        arrange();
+        //3 years and 2 quarters ahead
+        Quarter quarter2 = new Quarter(4,2026);
+        assertEquals(-3, quarter.compareTo(quarter2));
+    }
+
+    @Test
+    public void testCompareTo7() {
+        arrange();
+        Quarter quarter2 = new Quarter(1,2009);
+        assertEquals(14, quarter.compareTo(quarter2));
+    }
 }
